c9b09832731c2402f2dca7b72e6efb89149e15c9
cocomeh <coralie.perez98@orange.fr>
2024-02-06 14:23:03 +0100

    couverture Jacoco done 83%

--- src/test/java/safetyNet/safetyNet/FireStationControllerTest.java
+++ src/test/java/safetyNet/safetyNet/FireStationControllerTest.java
@@ -1,14 +1,65 @@
 package safetyNet.safetyNet;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import safetyNet.safetyNet.controler.FireStationController;
+import safetyNet.safetyNet.model.FireStation;
+import safetyNet.safetyNet.repository.FireStationRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 @SpringBootTest
 public class FireStationControllerTest {
 
     @Autowired
     private FireStationController fireStationController;
+
+    @Autowired
+    private FireStationRepository fireStationRepository;
+
+    @Test
+    public void createFireStationTest(){
+        List<FireStation> fireStationList= fireStationRepository.fireStationList();
+        int sizeStart = fireStationList.size();
+        FireStation newFireStation= new FireStation("Mehland","5");
+
+        fireStationController.createFireStation(newFireStation);
+        int sizeEnd= fireStationList.size();
+
+        assertThat(sizeEnd).isGreaterThan(sizeStart);
+    }
+
+    @Test
+    public void fireStationListTest(){
+        assertThat(fireStationController.fireStationList()).isNotNull();
+    }
+
+    @Test
+    public void updateFireStationTest(){
+        List<FireStation> fireStationList= fireStationRepository.fireStationList();
+        String address = "1509 Culver St";
+        FireStation fireStation = new FireStation("Meh","1");
+        assertThat(fireStationController.updateFireStation(address, fireStation).getStation().equals("5"));
+    }
+
+    @Test
+    public void deleteFireStationTest(){
+        List<FireStation> fireStationList= fireStationRepository.fireStationList();
+        int sizeStart = fireStationList.size();
+
+        fireStationController.deleteFireStation("1509 Culver St");
+
+        int sizeEnd= fireStationList.size();
+
+        assertThat(sizeStart).isGreaterThan(sizeEnd);
+    }
+
+
 
 
 }
--- src/test/java/safetyNet/safetyNet/MedicalRecordControllerTest.java
+++ src/test/java/safetyNet/safetyNet/MedicalRecordControllerTest.java
@@ -1,7 +1,63 @@
 package safetyNet.safetyNet;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import safetyNet.safetyNet.controler.MedicalRecordController;
+import safetyNet.safetyNet.model.MedicalRecord;
+import safetyNet.safetyNet.repository.MedicalRecordRepository;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 public class MedicalRecordControllerTest {
+
+    @Autowired
+    private MedicalRecordController medicalRecordController;
+
+    @Autowired
+    private MedicalRecordRepository medicalRecordRepository;
+
+    @Test
+    public void addMedicalRecordTest(){
+        List<MedicalRecord> medicalRecordList= medicalRecordRepository.medicalRecordList();
+        int sizeStart = medicalRecordList.size();
+        MedicalRecord newMedicalRecord= new MedicalRecord("Melissa","Meh","01/01/01", Collections.singletonList(""), Collections.singletonList(""));
+        medicalRecordController.addMedicalRecord(newMedicalRecord);
+        int sizeEnd = medicalRecordList.size();
+
+        assertThat(sizeEnd).isGreaterThan(sizeStart);
+    }
+
+    @Test
+    public void getAllMedicalRecordTest(){
+        assertThat(medicalRecordController.getAllMedicalRecord()).isNotNull();
+    }
+
+    @Test
+    public void updateMedicalRecordTest(){
+        List<MedicalRecord> medicalRecordList= medicalRecordRepository.medicalRecordList();
+        String firstName = "John";
+        String lastName = "Boyd";
+
+        MedicalRecord medicalRecord= new MedicalRecord("Melissa","Meh","01/01/01", Collections.singletonList(""), Collections.singletonList(""));
+
+        assertThat(medicalRecordController.updateMedicalRecord(firstName,lastName,medicalRecord).getBirthdate().equals("01/01/01"));
+    }
+
+    @Test
+    public void deleteMedicalRecordTest(){
+        List<MedicalRecord> medicalRecordList= medicalRecordRepository.medicalRecordList();
+        int sizeStart = medicalRecordList.size();
+        medicalRecordController.deleteMedicalRecord("John","Boyd");
+
+        int sizeEnd = medicalRecordList.size();
+
+        assertThat(sizeStart).isGreaterThan(sizeEnd);
+    }
+
 }
